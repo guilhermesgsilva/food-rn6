38495238af5cd9423dfb1e1b50e4fc5a72f914da
Guilherme Silva <silva.gsg@gmail.com>
2023-09-06 20:19:27 +0100

    feat: Recycle food-rn wih react Navigation v6

---
+++ src/components/BusinessesList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import BusinessListItem from "./BusinessListItem";
+
+const BusinessesList = ({ title, businesses }) => {
+  const navigation = useNavigation();
+
+  if (!businesses.length) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        {title} ({businesses.length})
+      </Text>
+      {/* <Text style={styles.subtitle}>Businesses: {businesses.length}</Text> */}
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={businesses}
+        keyExtractor={(business) => business.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("BusinessDetail", { id: item.id })
+              }
+            >
+              <BusinessListItem data={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  /* subtitle: {
+    marginLeft: 15,
+  }, */
+});
+
+export default BusinessesList;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ searchTerm, setSearchTerm, onSearchSubmit }) => {
+  return (
+    <View style={styles.background}>
+      <Feather name="search" style={styles.icon} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Search"
+        style={styles.input}
+        value={searchTerm}
+        onChangeText={(newSearchTerm) => setSearchTerm(newSearchTerm)}
+        onEndEditing={() => onSearchSubmit(searchTerm)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    margin: 15,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    flexDirection: "row",
+  },
+  icon: { fontSize: 35, alignSelf: "center", marginHorizontal: 15 },
+  input: { flex: 1, fontSize: 18 },
+});
+
+export default SearchBar;
